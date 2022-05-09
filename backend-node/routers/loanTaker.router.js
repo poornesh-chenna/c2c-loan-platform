fabe50cea950c1d0fd981c44b7dfb4bd0cdfc9d4
poornesh <poorneshchenna29@gmail.com>
2022-05-10 02:40:13 +0530

    acceptmodified bug fix

--- backend-node/routers/loanTaker.router.js
+++ backend-node/routers/loanTaker.router.js
@@ -8,75 +8,76 @@ import { BadRequestError } from '../CustomErrors/BadRequestError.js'
 const router = Router()
 
 router.post('/apply-loan', authorizeUser, async (req, res) => {
-    if (!(await isValidAmount(req.body.amount))) {
-        throw new BadRequestError('Amount limit is exceeded')
-    }
-    const newLoan = new Loan({
-        user_id: req.userId,
-        Amount: req.body.amount,
-        Tenure: req.body.Tenure,
-        Interest_Rate: req.body.Interest_Rate,
-    })
-    await newLoan.save()
-    res.status(200).send('successfully applied for loan')
+  if (!(await isValidAmount(req.body.amount))) {
+    throw new BadRequestError('Amount limit is exceeded')
+  }
+  const newLoan = new Loan({
+    user_id: req.userId,
+    Amount: req.body.amount,
+    Tenure: req.body.Tenure,
+    Interest_Rate: req.body.Interest_Rate,
+  })
+  await newLoan.save()
+  res.status(200).send('successfully applied for loan')
 })
 
 router.post('/accept-modified-loan', authorizeUser, async (req, res) => {
-    const loan = await Loan.findOne({ _id: req.body.loanId })
-    const acceptedLoans = await new AcceptedLoans({
-        borrower_id: req.userId,
-        lender_id: loan.user_id,
-        loan_id: req.body.loanId,
-    }).save()
-    const modified_id = req.body.modified_id
-    const modifiedDoc = loan.modified.find((doc) => {
-        return doc._id.toString() === modified_id
-    })
+  const loan = await Loan.findOne({ _id: req.body.loanId })
+  const modified_id = req.body.modified_id
+  const modifiedDoc = loan.modified.find((doc) => {
+    return doc._id.toString() === modified_id
+  })
 
-    modifiedDoc.status = 'Accepted'
-    loan.Tenure = req.body.tenure
-    loan.Interest_Rate = req.body.interest_rate
-    loan.status = 'Sanctioned'
-    await loan.save()
-    res.status(200).send({
-        message: 'You are now Successfully accepted the loan',
-    })
+  const acceptedLoans = await new AcceptedLoans({
+    borrower_id: req.userId,
+    lender_id: modifiedDoc.modified_user_id,
+    loan_id: req.body.loanId,
+  }).save()
+
+  modifiedDoc.status = 'Accepted'
+  loan.Tenure = req.body.tenure
+  loan.Interest_Rate = req.body.interest_rate
+  loan.status = 'Sanctioned'
+  await loan.save()
+  res.status(200).send({
+    message: 'You are now Successfully accepted the loan',
+  })
 })
 
 router.post('/reject-modified-loan', authorizeUser, async (req, res) => {
-    console.log(req.body)
-    const loan = await Loan.updateOne(
-        { _id: req.body.loanId },
-        {
-            $pull: {
-                modified: {
-                    _id: req.body.modified_id,
-                },
-            },
-        }
-    )
-    console.log(loan)
-    res.send({ message: 'modified request is rejected' })
-    // const modifiedDoc = loan.modified.find((doc) => {
-    //   return doc._id.toString() === modified_id;
-    // });
-    // modifiedDoc.status = 'Rejected';
+  console.log(req.body)
+  const loan = await Loan.updateOne(
+    { _id: req.body.loanId },
+    {
+      $pull: {
+        modified: {
+          _id: req.body.modified_id,
+        },
+      },
+    }
+  )
+  console.log(loan)
+  res.send({ message: 'modified request is rejected' })
+  // const modifiedDoc = loan.modified.find((doc) => {
+  //   return doc._id.toString() === modified_id;
+  // });
+  // modifiedDoc.status = 'Rejected';
 })
 
 router.get('/loan-requests', authorizeUser, async (req, res) => {
-    const allLoans = await Loan.find({
-        user_id: { $ne: req.userId },
-        status: 'Pending',
-    })
-    res.status(200).send(allLoans)
+  const allLoans = await Loan.find({
+    user_id: { $ne: req.userId },
+    status: 'Pending',
+  })
+  res.status(200).send(allLoans)
 })
 
 router.get('/myloans', authorizeUser, async (req, res) => {
-    const myloans = await Loan.find({ user_id: req.userId }).populate(
-        'modified.modified_user_id',
-        'username _id profile_image cibil'
-    )
-    res.status(200).send({ myloans })
+  const myloans = await Loan.find({ user_id: req.userId }).populate(
+    'modified.modified_user_id',
+    'username _id profile_image cibil'
+  )
+  res.status(200).send({ myloans })
 })
 
 export const LoanTakerRoutes = router
