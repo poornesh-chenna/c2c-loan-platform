092208aec6cd6d7b5a30345c787ac39c12c996a7
partheev <partheev8@gmail.com>
2022-05-15 13:46:41 +0530

    mail servie changed to gmail

--- backend-node/controllers/nodemailer/transporter.js
+++ backend-node/controllers/nodemailer/transporter.js
@@ -1,11 +1,15 @@
 import nodemailer from 'nodemailer'
 const options = {
-    host: process.env.SMTP_HOST || 'smtp-relay.sendinblue.com',
-    port: process.env.SMTP_PORT || 587,
-    secure: false, // true for 465, false for other ports
+    // host: process.env.SMTP_HOST || 'smtp-relay.sendinblue.com',
+    // port: process.env.SMTP_PORT || 587,
+    // secure: false, // true for 465, false for other ports
+    service: process.env.SMTP_SERVICE,
     auth: {
         user: process.env.MAIL_USER || 'partheev8@gmail.com', // generated ethereal user
         pass: process.env.MAIL_PW || 'OJAp1skFTG2gKZ9r', // generated ethereal password
+    },
+    tls: {
+        rejectUnauthorized: false,
     },
 }
 
